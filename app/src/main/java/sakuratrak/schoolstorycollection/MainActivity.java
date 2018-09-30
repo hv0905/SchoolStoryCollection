fe738a295514c03706aed7c9260f3e674b1951db
Saber <hv0905@outlook.com>
2018-09-30 22:35:32 +0800

    rebuild the layout

--- app/src/main/java/sakuratrak/schoolstorycollection/MainActivity.java
+++ app/src/main/java/sakuratrak/schoolstorycollection/MainActivity.java
@@ -1,34 +1,37 @@
 package sakuratrak.schoolstorycollection;
 
-import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView _mTextMessage;
     private RecyclerView _itemList;
     private FloatingActionButton _addItemBtn;
+
+    private ConstraintLayout _workbookLayout;
+    private ConstraintLayout _quizLayout;
+    private ConstraintLayout _settingLayout;
+    private BottomNavigationView _navigation;
+    private Button _tempbtn;
+
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,13 +40,19 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    //_mTextMessage.setText(R.string.title_home);
+                    _workbookLayout.setVisibility(View.VISIBLE);
+                    _quizLayout.setVisibility(View.INVISIBLE);
+                    _settingLayout.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_dashboard:
-                    //_mTextMessage.setText(R.string.title_quiz);
+                    _workbookLayout.setVisibility(View.INVISIBLE);
+                    _quizLayout.setVisibility(View.VISIBLE);
+                    _settingLayout.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_notifications:
-                    //_mTextMessage.setText(R.string.title_settings);
+                    _workbookLayout.setVisibility(View.INVISIBLE);
+                    _quizLayout.setVisibility(View.INVISIBLE);
+                    _settingLayout.setVisibility(View.VISIBLE);
                     return true;
             }
             return false;
@@ -54,42 +63,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        _mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottomNav);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        _mTextMessage = findViewById(R.id.message);
+        _navigation = findViewById(R.id.bottomNav);
         _addItemBtn = findViewById(R.id.addItemBtn);
+        _itemList = findViewById(R.id.itemList);
+        _workbookLayout = findViewById(R.id.workbookLayout);
+        _quizLayout = findViewById(R.id.quizLayout);
+        _settingLayout = findViewById(R.id.settingLayout);
+        _tempbtn = findViewById(R.id.tempbtn);
+
+        _navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         _addItemBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //navigate to add activity
                 //Snackbar.make(v,"Add button clicked!!!",2000).show();
-                AlertDialog ad = new AlertDialog.Builder(MainActivity.this).setTitle("选择题目类型")
+                AlertDialog ad = new AlertDialog.Builder(MainActivity.this).setTitle("选择题目类型").setIcon(R.drawable.ic_info_black_24dp)
                         .setItems(new String[]{"单选题", "多选题", "填空题","解答题"}, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
-                })
-//                        .setPositiveButton("完成", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        dialog.dismiss();
-//                    }
-//                })
-//                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialog, int which) {
-//                                dialog.dismiss();
-//                            }
-//                        })
-                        .show();
+                }).show();
 
             }
 
         });
-        _itemList = findViewById(R.id.itemList);
+
+        _tempbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,QuestionDetailActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
 
         ArrayList<String> lstr = new ArrayList<>();
         for (int i = 0;i<100;i++){
