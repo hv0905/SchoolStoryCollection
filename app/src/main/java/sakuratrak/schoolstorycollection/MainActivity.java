3e58ef290358c7ae59c8b30e95f77ab52b02f1b9
Saber <hv0905@outlook.com>
2018-10-01 18:55:46 +0800

    choose subject at the top of the toolbar

--- app/src/main/java/sakuratrak/schoolstorycollection/MainActivity.java
+++ app/src/main/java/sakuratrak/schoolstorycollection/MainActivity.java
@@ -8,16 +8,17 @@ import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
-import javax.security.auth.Subject;
 
 import sakuratrak.schoolstorycollection.core.LearningSubject;
 import sakuratrak.schoolstorycollection.core.QuestionType;
@@ -28,33 +29,40 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView _itemList;
     private FloatingActionButton _addItemBtn;
 
+    private AppCompatSpinner _subjectSpinner;
+
     private ConstraintLayout _workbookLayout;
     private ConstraintLayout _quizLayout;
+    private ConstraintLayout _unitLayout;
     private ConstraintLayout _settingLayout;
     private BottomNavigationView _navigation;
     private Button _unitManageBtn;
     private Button _tempbtn;
+    private Toolbar _toolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            _workbookLayout.setVisibility(View.INVISIBLE);
+            _quizLayout.setVisibility(View.INVISIBLE);
+            _unitLayout.setVisibility(View.INVISIBLE);
+            _settingLayout.setVisibility(View.INVISIBLE);
+            _subjectSpinner.setVisibility(View.VISIBLE);
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     _workbookLayout.setVisibility(View.VISIBLE);
-                    _quizLayout.setVisibility(View.INVISIBLE);
-                    _settingLayout.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_dashboard:
-                    _workbookLayout.setVisibility(View.INVISIBLE);
                     _quizLayout.setVisibility(View.VISIBLE);
-                    _settingLayout.setVisibility(View.INVISIBLE);
                     return true;
-                case R.id.navigation_notifications:
-                    _workbookLayout.setVisibility(View.INVISIBLE);
-                    _quizLayout.setVisibility(View.INVISIBLE);
+                case R.id.navigation_settings:
                     _settingLayout.setVisibility(View.VISIBLE);
+                    _subjectSpinner.setVisibility(View.INVISIBLE);
+                    return true;
+                case R.id.navigation_unit:
+                    _unitLayout.setVisibility(View.VISIBLE);
                     return true;
             }
             return false;
@@ -74,9 +82,14 @@ public class MainActivity extends AppCompatActivity {
         _itemList = findViewById(R.id.itemList);
         _workbookLayout = findViewById(R.id.workbookLayout);
         _quizLayout = findViewById(R.id.quizLayout);
+        _unitLayout = findViewById(R.id.unitLayout);
         _settingLayout = findViewById(R.id.settingLayout);
         _tempbtn = findViewById(R.id.tempbtn);
         _unitManageBtn = findViewById(R.id.main_settings_unitManageBtn);
+        _subjectSpinner = findViewById(R.id.subjectSpinner);
+        _toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(_toolbar);
 
         _navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         _addItemBtn.setOnClickListener(new View.OnClickListener() {
@@ -149,6 +162,10 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0;i<100;i++){
             lstr.add(String.format("item %d",i));
         }
+        ArrayAdapter<CharSequence> subjectDropdown = ArrayAdapter.createFromResource(this,R.array.learning_subjects,R.layout.layout_spinner_item);
+        subjectDropdown.setDropDownViewResource(R.layout.layout_spinner_dropdown);
+
+        _subjectSpinner.setAdapter(subjectDropdown);
 
         QuestionItemAdapter qia = new QuestionItemAdapter(lstr);
         _itemList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
