4118e43ce40de57328b22b98572e4c65f33e8bfe
Saber <hv0905@outlook.com>
2018-10-24 21:50:01 +0800

    addSplash

--- ImageEditor/src/main/java/me/kareluo/imaging/IMGEditBaseActivity.java
+++ ImageEditor/src/main/java/me/kareluo/imaging/IMGEditBaseActivity.java
@@ -46,6 +46,10 @@ abstract class IMGEditBaseActivity extends Activity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
         Bitmap bitmap = getBitmap();
         if (bitmap != null) {
             setContentView(R.layout.image_edit_activity);
---
+++ app/src/main/java/sakuratrak/schoolstorycollection/SplashActivity.java
@@ -0,0 +1,25 @@
+package sakuratrak.schoolstorycollection;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1000);
+    }
+}
