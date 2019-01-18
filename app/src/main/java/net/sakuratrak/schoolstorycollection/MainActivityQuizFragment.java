fe58c6f51cf3c6f49df1aaa5d52a3255d849da67
Saber <hv0905@outlook.com>
2019-01-18 17:26:29 +0800

    add log display in QuizFragnment

--- app/src/main/java/net/sakuratrak/schoolstorycollection/MainActivityQuizFragment.java
+++ app/src/main/java/net/sakuratrak/schoolstorycollection/MainActivityQuizFragment.java
@@ -8,25 +8,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import net.sakuratrak.schoolstorycollection.core.DbManager;
+import net.sakuratrak.schoolstorycollection.core.ExerciseLog;
+import net.sakuratrak.schoolstorycollection.core.ListDataProvider;
 import net.sakuratrak.schoolstorycollection.core.QuestionInfo;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 
 public final class MainActivityQuizFragment extends Fragment {
 
     public static final String TAG = "MainWindow_Quiz";
 
     ViewGroup _root;
-    ConstraintLayout _operateButtons;
-    View _quickQuizButton;
-    View _unitQuizButton;
-
-    final MainActivity.RequireRefreshEventHandler _update = this::update;
+    private ConstraintLayout _operateButtons;
+    private View _quickQuizButton;
+    private View _unitQuizButton;
+    private RecyclerView _listLog;
+    private ExerciseLogAdapter _mainAdapter;
+    private ArrayList<ExerciseLogAdapter.DataContext> _logContext;
+    private final MainActivity.RequireRefreshEventHandler _update = this::update;
 
     public MainActivityQuizFragment() {
         // Required empty public constructor
@@ -41,6 +49,7 @@ public final class MainActivityQuizFragment extends Fragment {
         _operateButtons = _root.findViewById(R.id.operateButtons);
         _quickQuizButton = _root.findViewById(R.id.quickQuizBtn);
         _unitQuizButton = _root.findViewById(R.id.unitQuizBtn);
+        _listLog = _root.findViewById(R.id.listLog);
         //_buttonTest = _root.findViewById(R.id.buttonTest);
 
         _quickQuizButton.setOnClickListener(v -> {
@@ -71,6 +80,10 @@ public final class MainActivityQuizFragment extends Fragment {
         });
 
         //_buttonTest.setOnClickListener(v -> AlarmReceiver.setupAlarm(getContext(),false));
+        _logContext = new ArrayList<>();
+        _mainAdapter = new ExerciseLogAdapter(new ListDataProvider<>(_logContext));
+        _listLog.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        _listLog.setAdapter(new AlphaInAnimationAdapter(_mainAdapter));
         getParent().addRequireRefreshEvent(_update);
         update();
         return _root;
@@ -83,8 +96,37 @@ public final class MainActivityQuizFragment extends Fragment {
         super.onDestroyView();
     }
 
-    public void update() {
+    private void update() {
         _operateButtons.setBackgroundColor(UiHelper.getFlatUiColor(getParent(), getParent().getCurrentSubject().getId()));
+
+        //载入记录
+        _logContext.clear();
+        try {
+            List<ExerciseLog> datas = DbManager.getDefaultHelper(getContext()).getExerciseLogs().queryForAll();
+            int index = 1;
+            UUID groupUuid = null;
+            for (ExerciseLog data :
+                    datas) {
+                if (data.getGroupGuid().equals(groupUuid)) {
+                    index++;
+                } else {
+                    index = 1;
+                    groupUuid = data.getGroupGuid();
+                }
+                _logContext.add(new ExerciseLogAdapter.DataContext(index,
+                        data.getQuestion().getTitle(),
+                        data.getQuestion().getUnit() == null ? getString(R.string.emptyUnit) : data.getQuestion().getUnit().getName(),
+                        data.getCorrectRatio(),
+                        v -> {
+                            Intent intent = new Intent(getContext(),QuestionDetailActivity.class);
+                            intent.putExtra(QuestionDetailActivity.EXTRA_QUESTION_ID,data.getQuestion().getId());
+                            startActivity(intent);
+                        }));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
