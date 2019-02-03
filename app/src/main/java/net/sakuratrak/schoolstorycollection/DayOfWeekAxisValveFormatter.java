46a607bf6a25dca1e0fee352f61b2d05c83539d3
Saber <hv0905@outlook.com>
2019-02-03 12:44:25 +0800

    Create DayOfWeekAxisValveFormatter.java

---
+++ app/src/main/java/net/sakuratrak/schoolstorycollection/DayOfWeekAxisValveFormatter.java
@@ -0,0 +1,19 @@
+package net.sakuratrak.schoolstorycollection;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+
+import java.util.Calendar;
+
+
+public class DayOfWeekAxisValveFormatter implements IAxisValueFormatter {
+
+    public static final String[] WEEK_CHINESE = {"日", "一", "二", "三", "四", "五", "六"};
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        Calendar currentDay = Calendar.getInstance();
+        currentDay.add(Calendar.DAY_OF_YEAR,(int)(1-value));
+        return "星期" + WEEK_CHINESE[currentDay.get(Calendar.DAY_OF_WEEK) - 1];
+    }
+}
